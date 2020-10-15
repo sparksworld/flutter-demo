fa8831395af986aa73b68bb57168de1915317fd0
spark <spark.xiaoyu@qq.com>
2020-10-15 20:13:41 +0800

    Fix splash

--- android/app/src/main/kotlin/com/example/flutterdemo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterdemo/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.flutterdemo
 import io.flutter.embedding.android.FlutterActivity
 
 import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.ViewTreeObserver
 import android.view.WindowManager
 class MainActivity: FlutterActivity() {
+
 }
