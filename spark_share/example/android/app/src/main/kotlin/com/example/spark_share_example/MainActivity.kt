42949a5f794641f28d110f91c2665adeeb208bb7
spark <spark.xiaoyu@qq.com>
2020-10-27 09:31:55 +0800

    del spark_spark

--- spark_share/example/android/app/src/main/kotlin/com/example/spark_share_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.spark_share_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- spark_share/ios/Classes/SparkSharePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface SparkSharePlugin : NSObject<FlutterPlugin>
-@end
